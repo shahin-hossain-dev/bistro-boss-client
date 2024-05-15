e2d69e165b08e25b4b4117b2298e8ffef2af87a5
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-05-15 22:00:40 +0600

    64-1 Module Introduction And Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Bistro Boss</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+    </>
+  );
+}
+
+export default App;
