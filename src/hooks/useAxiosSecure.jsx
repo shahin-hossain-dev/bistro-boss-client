235fdf7eab24d89fd920035b6ccfe5e7035c87ea
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-05-23 19:13:20 +0600

    68-8 (Advanced) Verify Token And Axios Interceptor

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,6 +5,28 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
+  axiosSecure.interceptors.request.use(function (config) {
+    const token = localStorage.getItem("access-token");
+    console.log("request stopped by interceptor", token);
+    config.headers.authorization = `Bearer ${token}`;
+    return config;
+  }),
+    function (error) {
+      return Promise.reject(error);
+    };
+
+  // interceptor 401 and 401 status
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      const status = error.response.status;
+      console.log("status error", error.response.status);
+      return Promise.reject(error);
+    }
+  );
+
   return axiosSecure;
 };
 
